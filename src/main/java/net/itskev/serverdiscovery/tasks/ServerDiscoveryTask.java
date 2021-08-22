23562d5606e7e78ea826c4b5ba6df297eec50006
Kevin Huber <kevin.huber@hotmail.com>
2021-08-22 20:39:49 +0200

    Change to annotation based discovery

--- src/main/java/net/itskev/serverdiscovery/tasks/ServerDiscoveryTask.java
+++ src/main/java/net/itskev/serverdiscovery/tasks/ServerDiscoveryTask.java
@@ -1,12 +1,14 @@
 package net.itskev.serverdiscovery.tasks;
 
-import io.fabric8.kubernetes.api.model.Endpoints;
+import io.fabric8.kubernetes.api.model.Namespace;
+import io.fabric8.kubernetes.api.model.NamespaceList;
+import io.fabric8.kubernetes.api.model.Pod;
+import io.fabric8.kubernetes.api.model.PodList;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import lombok.RequiredArgsConstructor;
 import net.md_5.bungee.api.ProxyConfig;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.config.ServerInfo;
-import net.md_5.bungee.config.Configuration;
 
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
@@ -20,19 +22,22 @@ public class ServerDiscoveryTask extends TimerTask {
 
   private final ProxyServer proxyServer;
   private final DefaultKubernetesClient kubernetesClient;
-  private final Configuration configuration;
 
   @Override
   public void run() {
-    List<ServerInfo> serverInfo = new ArrayList<>();
-    for (String namespace : getNamespaces()) {
-      try {
-        Endpoints endpoints = kubernetesClient.endpoints().inNamespace(namespace).withName(getEndpointName()).get();
-        if (endpoints == null) {
-          return;
-        }
+    NamespaceList list = kubernetesClient.namespaces()
+        .withLabel("server-discovery", "true")
+        .list();
 
-        serverInfo.addAll(getServerInfo(endpoints));
+    List<ServerInfo> serverInfo = new ArrayList<>();
+    for (Namespace namespace : list.getItems()) {
+      try {
+        PodList podList = kubernetesClient.pods()
+            .inNamespace(namespace.getMetadata().getName())
+            .withLabel("server-discovery", "true")
+            .list();
+
+        serverInfo.addAll(getServerInfo(podList.getItems()));
       } catch (Exception exception) {
         proxyServer.getLogger().log(Level.SEVERE, "Failed to get servers", exception);
       }
@@ -43,21 +48,11 @@ public class ServerDiscoveryTask extends TimerTask {
     config.addServers(serverInfo);
   }
 
-  private String getEndpointName() {
-    return configuration.getString("endpointName");
-  }
-
-  private List<String> getNamespaces() {
-    return configuration.getStringList("namespaces");
-  }
-
-  private List<ServerInfo> getServerInfo(Endpoints endpoints) {
-    return endpoints.getSubsets().stream()
-        .filter(endpointSubset -> !endpointSubset.getAddresses().isEmpty())
-        .flatMap(endpointSubset -> endpointSubset.getAddresses().stream())
-        .map(endpointAddress -> proxyServer.constructServerInfo(
-            endpointAddress.getTargetRef().getName(),
-            new InetSocketAddress(endpointAddress.getIp(), 25565),
+  private List<ServerInfo> getServerInfo(List<Pod> pods) {
+    return pods.stream()
+        .map(pod -> proxyServer.constructServerInfo(
+            pod.getMetadata().getName(),
+            new InetSocketAddress(pod.getStatus().getPodIP(), 25565),
             "",
             false))
         .collect(Collectors.toList());
